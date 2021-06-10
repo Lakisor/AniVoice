32ee9537f4042b297f86395ee9823a28a8a226ce
Lokisor <lokizor@mail.ru>
2021-06-10 21:25:09 +0300

    master

--- Voicer/models.py
+++ Voicer/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Anime(models.Model):
+    title = models.CharField(max_length=350)
+    pic_title = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.title
